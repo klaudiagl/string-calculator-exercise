45a6cf1ef92b86ec2711ccf292415fc78df1d9eb
Klaudia <klaudiaglocka@gmail.com>
2025-05-30 16:10:45 +0200

    feat: support parsing and summing one or multiple comma-separated numbers
    - Extended the add method to handle:
     - a single number
     - two numbers separated by a comma
     - an unknown number of comma-separated values
    - Implemented basic input parsing and integer summation
    - Added basic validation and fallback behavior for invalid or empty input numbers

--- string-calculator/src/main/java/com/example/stringcalculator/service/StringCalculatorService.java
+++ string-calculator/src/main/java/com/example/stringcalculator/service/StringCalculatorService.java
@@ -1,6 +1,9 @@
 package com.example.stringcalculator.service;
 
+import com.example.stringcalculator.exceptions.NumberExpectedException;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class StringCalculatorService {
@@ -9,7 +12,17 @@ public class StringCalculatorService {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .filter(string -> !string.isEmpty())
+                .mapToInt(value -> {
+                    try {
+                        return Integer.parseInt(value);
+                    } catch (NumberFormatException e) {
+                        throw new RuntimeException(new NumberExpectedException("Invalid number: " + value));
+                    }
+                }).sum();
     }
 
 }
--- string-calculator/src/test/java/com/example/stringcalculator/StringCalculatorServiceTest.java
+++ string-calculator/src/test/java/com/example/stringcalculator/StringCalculatorServiceTest.java
@@ -1,11 +1,11 @@
 package com.example.stringcalculator;
 
+import com.example.stringcalculator.exceptions.NumberExpectedException;
 import com.example.stringcalculator.service.StringCalculatorService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorServiceTest {
 
@@ -18,9 +18,37 @@ public class StringCalculatorServiceTest {
     }
 
     @Test
-    void shouldReturnZeroForEmptyString() {
+    void testAddEmptyStringReturnsZero() {
         assertEquals(0, calculatorService.add(""));
         assertEquals(0, calculatorService.add(null));
+    }
+
+    @Test
+    void testAddSingleNumberReturnsSameNumber() {
+        assertEquals(1, calculatorService.add("1"));
+    }
+
+    @Test
+    void testAddTwoNumbersReturnsTheirSum() {
+        assertEquals(3, calculatorService.add("1,2"));
+
+    }
+
+    @Test
+    void testAddMultipleNumbersReturnsSum() {
+        assertEquals(12, calculatorService.add("1,2,4,5"));
+    }
+
+    @Test
+    void testThrowExceptionWhenInputContainsInvalidNumber() {
+        Exception exception = assertThrows(Exception.class, () -> calculatorService.add("1,a,2"));
+
+        if (exception instanceof NumberExpectedException) {
+            assertTrue(exception.getMessage().contains("Invalid number: a"));
+        } else if (exception instanceof RuntimeException) {
+            assertTrue(exception.getCause() instanceof NumberExpectedException);
+            assertTrue(exception.getCause().getMessage().contains("Invalid number: a"));
+        }
     }
 
 
